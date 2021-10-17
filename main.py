ad29c12d6579e3d44aff19bde002076a4fa3ce6e
Xipher <yukino.konata@gmail.com>
2021-10-18 02:52:37 +0900

    v0.1

--- main.py
+++ main.py
@@ -2,7 +2,22 @@
 
 from __future__ import unicode_literals
 import yt_dlp
+import subprocess
+import sys
+import os
+import yaml
 
-ydl_opts = {}
+CONFIG_PATH = ".\yt-dlp_conf.yaml"
+
+with open(CONFIG_PATH) as f:
+    CONFIG_DICT = yaml.safe_load(f)
+
+CHANNEL_PATH = ".\channels.yaml"
+with open(CHANNEL_PATH) as f:
+    CHANNEL_DICT = yaml.safe_load(f)
+
+ydl_opts = CONFIG_DICT
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+    ydl.download(CHANNEL_DICT)
+    # ydl.download(['https://www.youtube.com/watch?v=Rk6rtktdDb4'])
+    # ydl.download(['https://www.youtube.com/channel/UCM_I_CWRcp20EkrAKa7HUlA'])
