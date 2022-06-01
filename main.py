3d06bd0c48ca840f9e238b0d420a69152e268126
Xipher <yukino.konata@gmail.com>
2022-06-01 12:45:24 +0900

    add: CONFIG, CHANNEL PATH argument

--- main.py
+++ main.py
@@ -6,15 +6,30 @@ import subprocess
 import sys
 import os
 import yaml
+import argparse
 
-CONFIG_PATH = os.path.dirname(__file__)+"/yt-dlp_conf.yaml"
-with open(CONFIG_PATH,encoding="utf-8") as f:
-    CONFIG_DICT = yaml.safe_load(f)
+# コマンドライン引数解析
+parser = argparse.ArgumentParser()
 
-CHANNEL_PATH = os.path.dirname(__file__)+"/channels.yaml"
-with open(CHANNEL_PATH,encoding="utf-8") as f:
-    CHANNEL_DICT = yaml.safe_load(f)
+parser.add_argument("--CONFIG_PATH")
+parser.add_argument("--CHANNEL_PATH")
+args = parser.parse_args()
 
+# 引数にファイルのPATH指定があればそれを使う、なければ実行ファイルと同じ階層に物を使う
+
+if args.CONFIG_PATH is None:
+    CONFIG_PATH = os.path.dirname(__file__)+"/yt-dlp_conf.yaml"
+    with open(CONFIG_PATH,encoding="utf-8") as f:
+        CONFIG_DICT = yaml.safe_load(f)
+else:
+    CONFIG_PATH = args.CONFIG_PATH
+
+if args.CHANNEL_PATH is None:
+    CHANNEL_PATH = os.path.dirname(__file__)+"/channels.yaml"
+    with open(CHANNEL_PATH,encoding="utf-8") as f:
+        CHANNEL_DICT = yaml.safe_load(f)
+else:
+    CHANNEL_PATH = args.CHANNEL_PATH
 
 if "match_filter" in CONFIG_DICT:
     match_filter_text = CONFIG_DICT["match_filter"]
